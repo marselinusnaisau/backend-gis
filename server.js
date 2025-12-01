2e789565730b71959f36af97a29c5dece2d360e7
714230011 <marselinusnaisau90@gmail.com>
2025-12-01 20:04:01 +0700

    add configuration Cors

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const mapRoutes = require('./src/routes/mapRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: "https://marselinusnaisau.github.io/frontend-gis",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization"
+}));
 
-app.use(cors());
 app.use(express.json());
 
 app.use('/api/locations', mapRoutes);
-
 
 app.get('/', (req, res) => {  
     res.send('Server Backend Leaflet Jalan 🚀');
